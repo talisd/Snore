9f5a6a52d3fe384f928ae60e0011faca4a1ed1d5
talisd <dima@dmitry-talismans-macbook-pro.local>
2015-11-15 15:08:22 -0500

    Initial Commit

---
+++ Snore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snore
+//
+//  Created by Dmitry Talisman on 11/15/15.
+//  Copyright © 2015 Dmitry Talisman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
